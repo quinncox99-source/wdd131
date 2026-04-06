07a592bde91c0c755e6cf24574603ea8e1703a64
quinncox99-source <quinncox99@gmail.com>
2026-04-06 12:57:36 -0600

    something

--- variables.js
+++ variables.js
@@ -1,39 +1,17 @@
+const LVL="<b>Level: </b>"
+let level=5
+const HP="<b>Health:</b>"
+let health=100
 
-const PI = 3.14;
-let radius = 3;
-                  
-let area = radius * radius * PI;
-console.log(area);
-
-radius = 20;
-
-area = radius * radius * PI;
-console.log(area);
-
-radius = 50;
-
-area = radius * radius * PI;
-console.log(area);
-
-
-//Type Coersion
-const one = 1;
-const two = "2";
-let result = one * two;
-console.log(result);
-
-result = one + two;
-console.log(result);
-
-
-//Scope
-
-let course = "CSE131"; //global scope
-if (true) {
-    let student = "John";
-    console.log(course);  //works just fine, course is global
-    console.log(student); //works just fine, it's being accessed within the block
+function levelUp() {
+level+=1;
+    document.getElementById("level").innerHTML = LVL+level;
 }
-console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
-                    
+function attacked() {
+    if(health<=0){
+        alert("this creature has died")
+    }else{
+    health-=20;
+    document.getElementById("hp").innerHTML = HP+health;
+    }
+}
